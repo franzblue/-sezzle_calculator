6b5d3258dc1ccd249af70b31f34bb7322f1f5539
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2021-05-12 22:05:30 -0500

    messing with package.json scripts to help

--- server/server.js
+++ server/server.js
@@ -5,12 +5,12 @@ const PORT = process.env.PORT || 5000;
 const OperationsRouter = require ('./routes/operations_router');
 // require('dotenv').config();
 
-app.use((req,res, next)=>{
-    res.setHeader('Access-Control-Allow-Origin',"http://localhost:3000");
-    res.setHeader('Access-Control-Allow-Headers',"*");
-    res.header('Access-Control-Allow-Credentials', true);
-    next();
-});
+// app.use((req,res, next)=>{
+//     res.setHeader('Access-Control-Allow-Origin',"http://localhost:3000");
+//     res.setHeader('Access-Control-Allow-Headers',"*");
+//     res.header('Access-Control-Allow-Credentials', true);
+//     next();
+// });
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
