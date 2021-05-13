2878a6db83db6b2129fb2a3ff08ee82d52c4cf46
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2021-05-12 21:30:52 -0500

    copy pasted the wrong code

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
 const webpack = require('webpack'); //to access built-in plugins
 
 module.exports = {
-    devServer: {
-        disableHostCheck: true,
-      },
+  module: {
+    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  
+  devServer: {
+    contentBase: [contentBasePublic, contentBaseOther],
+    contentBasePublicPath: [contentBasePublicPath, contentBasePublicOtherPath],
+  },
+
+  devServer: {
+    disableHostCheck: true,
+  },
 };
